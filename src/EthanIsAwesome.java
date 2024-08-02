aa37e791d245709339d9fdbe5f2ed05d9e7292ab
Ethan <ethangerber82@gmail.com>
2024-08-02 09:34:19 +0200

    Ethan is altyd awesome

--- src/EthanIsAwesome.java
+++ src/EthanIsAwesome.java
@@ -1,5 +1,9 @@
+
 public class EthanIsAwesome {
     public static void main(String[] args) {
-        System.out.println("Welcome to EthanIsAwesome!");
+        int i;
+        for (i = 0; i < 3; i++) {
+            System.out.println("Hello World");
+        }
     }
 }
